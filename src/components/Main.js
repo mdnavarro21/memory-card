d1ea8e5ae53e3aac1748599c2cfd0ba047f7be32
Matthew Dylan <dylannavarro65@gmail.com>
2022-07-02 18:00:28 -0400

    implemented a modal that displays a message whenver the player loses

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,12 @@
 import React, { useState }from "react";
 import Scoreboard from './main-components/Scoreboard';
 import Gameboard from './main-components/Gameboard';
+import Modal from "./main-components/Modal";
 import '../styles/Main.css'
 const Main = () => {
     const [currentScore, setCurrentScore] = useState(0);
     const [bestScore, setBestScore] = useState(0);
+    const [show, setShow] = useState(false);
 
     const handleCorrectGuess = () => {
         setCurrentScore(currentScore + 1);
@@ -19,11 +21,21 @@ const Main = () => {
         }
 
         setCurrentScore(0);
+        showModal();
     }
+
+    function showModal(){
+        setShow(true);
+    };
+    
+    const hideModal = () => {
+        setShow(false);
+    };
     return (
         <main>
             <Scoreboard currentScore = {currentScore} bestScore = {bestScore}/>
             <Gameboard handleCorrectGuess = {handleCorrectGuess} handleIncorrectGuess = {handleIncorrectGuess}/>
+            <Modal show={show} handleClose={ hideModal}></Modal>
         </main>
     )
 }
---
+++ src/components/main-components/Modal.js
@@ -0,0 +1,20 @@
+import '../../styles/Modal.css'
+import React from 'react';
+
+const Modal = ({ handleClose, show }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        <p>You lose, unfortunately.</p>
+        <p>Don't quit now though, with every loss you're improving. :)</p>
+        <p>Feel free to play again. :D</p>
+        <p>Just make sure that before you do: <br/>Take a deep breath,😤<br/>have a sip of water,🚰 <br/>go for a nice nature walk,🌲 <br/>and reset that mental!🧠</p>
+        <button onClick = {handleClose}>Close</button>
+      </section>
+    </div>
+  );
+};
+
+export default Modal;
